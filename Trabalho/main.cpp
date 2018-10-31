588336d09022f6d349118d13841a12be91b04367
Leonardo Holtz <leo.holtz98@gmail.com>
2018-10-30 22:19:38 -0300

    Modificação na main para a leitura do arquivo

--- Trabalho/main.cpp
+++ Trabalho/main.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-
 using namespace std;
 
 int main()
 {
-	string line = "oi";
-	long long int linhas = 0;
+	long int linhas = 0;
+	string line;
 	cout << "Acessando arquivo de dados..." << endl;
 	ifstream DataFile;
-	DataFile.open("perfil_eleitor_secao_2018_RS.csv");
+	DataFile.open("candidatos_2018_BRASIL.csv");
 	if(!DataFile)
 	{
     cerr << "O arquivo de dados não pode ser acessado!";
@@ -21,16 +20,12 @@ int main()
 		cout << "Arquivo de dados acessado com sucesso!" << endl;
 		while(!DataFile.eof())
 		{
-			line0 = line
 			getline(DataFile, line);
+			cout << line << endl;
 			linhas++;
 		}
+		cout << linhas << endl;
+		DataFile.close();
 	}
-	cout << linhas << endl;
-	cout << line0 << endl;
-	getline(DataFile, line);
-	linhas++;
-	cout << line << endl;
-	cout << linhas << endl;
 	return 0;
 }
