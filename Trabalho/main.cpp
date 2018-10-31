42b355d4ae46a940b6dae4d0f00c3d88f652f1bf
Leonardo Holtz <leo.holtz98@gmail.com>
2018-10-30 22:53:59 -0300

    Menu de iniciação começado

--- Trabalho/main.cpp
+++ Trabalho/main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <stdlib.h>
 using namespace std;
 
+/**************/
+/* CABEÇALHOS */
+/**************/
+void leitura_arquivo(); // Faz a leitura do arquivo onde estão os dados originalmente
+
 int main()
+{
+	system("CLS");
+	cout << "Bem vindo a pesquisa de candidatos das eleicoes de 2018!" << endl << endl;
+	cout << "Nela, voce pode verificar os dados de todos os candidatos dessa eleicao" << endl;
+	cout << "e tambem podera pesquisar e realizar estatisticas em cima dos candidatos" << endl << endl;
+	cout << "Por favor, selecione uma opcao abaixo:" <<endl;
+	return 0;
+}
+
+void leitura_arquivo()
 {
 	long int linhas = 0;
 	string line;
@@ -13,7 +29,7 @@ int main()
 	if(!DataFile)
 	{
     cerr << "O arquivo de dados não pode ser acessado!";
-    return -1;   // chama o sistema para interrromper o programa
+    //return -1;   // chama o sistema para interrromper o programa
 	}
 	else
 	{
@@ -27,5 +43,4 @@ int main()
 		cout << linhas << endl;
 		DataFile.close();
 	}
-	return 0;
 }
