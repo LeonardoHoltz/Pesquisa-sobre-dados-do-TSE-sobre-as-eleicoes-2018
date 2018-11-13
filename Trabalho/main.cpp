69181411898acd233590b8d3b9749c2683db2350
Leonardo Holtz <leo.holtz98@gmail.com>
2018-11-12 23:40:23 -0200

    ggrrr que bagulho bugado
    pela ultimavez meu irmao

--- Trabalho/main.cpp
+++ Trabalho/main.cpp
@@ -1,20 +1,44 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cstring>
 #include <stdlib.h>
 using namespace std;
+
+const int ALPHABET_TAM = 27;
+const int MAX_TAM = 30;
 
 /**************/
 /* CABEÇALHOS */
 /**************/
-int leitura_arquivo(); // Faz a leitura do arquivo onde estão os dados originalmente
+int abertura_arquivo(); // Faz a leitura do arquivo onde estão os dados originalmente
+void leitura_arquivo();
+
+class candidatos
+{
+	public: 
+		int numero; 
+		string nome, partido, situacao, cargo, UF;
+		
+};
+
+typedef struct
+{
+	candidato *filhos[ALPHABET_TAM];
+	bool eh_palavra; //flag que indica se o conteudo do nodo é uma palavra ou apenas um prefixo 
+}TrieNode;
+
+ifstream DataFile;
 
 /********/
 /* MAIN */
 /********/
 int main()
 {
-	int opcao, teste_erro = 0;
+	int opcao = 0;
+	int teste_erro;
+	
+	
 	
 	system("CLS");
 	cout << "Bem vindo a pesquisa de candidatos das eleicoes de 2018!" << endl << endl;
@@ -31,7 +55,7 @@ int main()
 		switch(opcao)
 		{
 			case 1:
-				teste_erro = leitura_arquivo();
+				teste_erro = abertura_arquivo();
 				if(teste_erro == -1)
 				{
 					cout << "Certifique-se que o arquivo esta no mesmo diretorio do programa" << endl << endl;
@@ -47,31 +71,69 @@ int main()
 				break;
 		}
 	} while((opcao != 1 && opcao != 2) || teste_erro == -1);
+	
+	leitura_arquivo();
+	
 	return 0;
 }
 
+<<<<<<< HEAD
 /***********/
 /* FUNÇÕES */
 /***********/
 
 int leitura_arquivo()
+=======
+
+
+int abertura_arquivo()
+>>>>>>> da8ec20b5aafd74d79e0aa31cd652b138171437a
 {
-	long int linhas = 0;
-	string line;
+	
 	cout << endl << "Acessando arquivo de dados..." << endl << endl;
-	ifstream DataFile;
+	
 	DataFile.open("candidatos_2018_BRASIL_updated.csv");
+	cout << "batata";
 	if(!DataFile)
 	{
-    cerr << "O arquivo de dados nao pode ser acessado!" << endl;
-    return -1;   // chama o sistema para interrromper o programa
+		cout << "nigga why isnt this shit workin ??";
+		cerr << "O arquivo de dados nao pode ser acessado!" << endl;
+		return -1;   // chama o sistema para interrromper o programa
 	}
 	else
 	{
 		cout << "Arquivo de dados acessado com sucesso!" << endl;
-		while(!DataFile.eof())
-			getline(DataFile, line);
-		DataFile.close();
+		
 		return 0;
 	}
 }
+
+void leitura_arquivo()
+{
+	string line;
+	
+	while(!(DataFile.eof()))
+	{
+		getline(DataFile, line, ';');// vai passando pelo arquivo, separando palavra por palavra com o token ';'
+		
+		
+		
+		//cout << line;, tempo de espera muito longo para printar tudo
+	}
+	
+	cout << "deu bom taoquei?";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
